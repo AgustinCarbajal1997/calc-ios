4e23fd16e6623c3ade91d73d93f5c968c58e971b
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-05 20:47:27 -0300

    [feature] outlets

--- IOS-Calculator/Controller/Home/HomeViewController.swift
+++ IOS-Calculator/Controller/Home/HomeViewController.swift
@@ -2,6 +2,37 @@ import UIKit
 
 final class HomeViewController: UIViewController {
 
+    // MARK - Outlets
+    
+    //result
+    @IBOutlet weak var resultNumber: UILabel!
+    
+    //numbers
+    @IBOutlet weak var number0: UIButton!
+    @IBOutlet weak var number1: UIButton!
+    @IBOutlet weak var number2: UIButton!
+    @IBOutlet weak var number3: UIButton!
+    @IBOutlet weak var number4: UIButton!
+    @IBOutlet weak var number5: UIButton!
+    @IBOutlet weak var number6: UIButton!
+    @IBOutlet weak var number7: UIButton!
+    @IBOutlet weak var number8: UIButton!
+    @IBOutlet weak var number9: UIButton!
+    @IBOutlet weak var operatorDecimal: UIButton!
+    
+    //operators
+    @IBOutlet weak var operatorAC: UIButton!
+    @IBOutlet weak var operatorPlusMinus: UIButton!
+    @IBOutlet weak var operatorPercent: UIButton!
+    @IBOutlet weak var operatorResult: UIButton!
+    @IBOutlet weak var operatorAddition: UIButton!
+    @IBOutlet weak var operatorSubstraction: UIButton!
+    @IBOutlet weak var operatorMultiplication: UIButton!
+    @IBOutlet weak var operatorDivision: UIButton!
+    
+    
+    
+    
     // MARK - initializing
     
     init(){
